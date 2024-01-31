2b59bb3633c05f03f08fd8fb82309236ec811f58
NDavidGR <146215925+NDavidGR@users.noreply.github.com>
2024-01-31 02:13:17 +0000

    random carg generator project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,47 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+  let number = Math.floor(Math.random() * 13 + 1);
+  switch (number) {
+    case 1:
+      number = "A";
+      break;
+    case 11:
+      number = "J";
+      break;
+    case 12:
+      number = "Q";
+      break;
+    case 13:
+      number = "K";
+      break;
+  }
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  let symbol = Math.floor(Math.random() * 4 + 1);
+  switch (symbol) {
+    case 1:
+      symbol = "♦";
+      break;
+    case 2:
+      symbol = "♥";
+      break;
+    case 3:
+      symbol = "♠";
+      break;
+    case 4:
+      symbol = "♣";
+      break;
+  }
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const element = document.querySelector(".number");
+  element.innerHTML = number;
+
+  const elementTop = document.querySelector(".top-icon");
+
+  const elementBottom = document.querySelector(".bottom-icon");
+
+  const color = Math.random() < 0.5 ? "red" : "black";
+  elementTop.innerHTML = symbol;
+  elementTop.style.color = color;
+
+  elementBottom.innerHTML = symbol;
+  elementBottom.style.color = color;
 };
